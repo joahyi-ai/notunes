91787cca7c9b7364b6bc7e5ff4a34481c20e243b
joahyi-ai <joahyi7@gmail.com>
2023-06-15 17:16:19 +0800

    first-commit:landing page

---
+++ app/pricing/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Pricing from "@/components/Pricing";
+
+export default function page() {
+  return (
+    <div>
+      <Header />
+      <Pricing />
+      <Footer />
+    </div>
+  );
+}
---
+++ app/privacy/page.jsx
@@ -0,0 +1,14 @@
+import Header from "@/components/Header";
+import Privacy from "@/components/Privacy";
+import React from "react";
+import Footer from "@/components/Footer";
+
+export default function page() {
+  return (
+    <div>
+      <Header />
+      <Privacy />
+      <Footer />
+    </div>
+  );
+}
